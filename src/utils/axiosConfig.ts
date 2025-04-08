9f9aee8298fc368c31ec1b61bfa3aa2e48e29355
JowelTisso <joweltisso@gmail.com>
2025-04-08 13:10:33 +0530

    fix : minor api fix

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const PROD_URL = "https://email-flow-be.onrender.com/";
+const PROD_URL = "https://email-flow-be.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: PROD_URL,
