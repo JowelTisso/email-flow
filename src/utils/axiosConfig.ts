7f17f881236d272178f4119e26fb292dba27e734
JowelTisso <joweltisso@gmail.com>
2025-04-08 12:48:53 +0530

    fix : added production url for api call

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const DEV_URL = "http://192.168.29.225:5000/api";
-// const PROD_URL =
-//   "https://preparation-time-tracker-backend.onrender.com/";
+const PROD_URL = "https://email-flow-be.onrender.com/";
 
 export const axiosInstance = axios.create({
-  baseURL: DEV_URL,
+  baseURL: PROD_URL,
 });
